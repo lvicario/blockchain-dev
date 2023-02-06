5fd0b19745a0b49f1b2580b0b355ca6b6de6058b
Leandro Vicario <leandro.vicario@gmail.com>
2023-02-06 21:26:08 +0800

    First smart contract

---
+++ contracts/Faucet.sol
@@ -0,0 +1,12 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Faucet {
+    // storage variables
+    uint public funds = 1000; // uint only positive values
+    int public counter = -10; // positive and negative values
+    uint32 public test = 4294967295;
+
+}
